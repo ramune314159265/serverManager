991e54799d43f5ab5e0086b56a817d9cef6b945c
ramune314159265 <ramune314159265@gmail.com>
2024-03-11 22:45:29 +0900

    start実装

--- src/api/v1/server.ts
+++ src/api/v1/server.ts
@@ -15,3 +15,26 @@ serverRouter.get('/', (req, res) => {
 
 	res.send(JSON.stringify(sendData))
 })
+
+serverRouter.get('/:serverId/start', (req, res) => {
+	if (!Object.hasOwn(Server.list, req.params.serverId)) {
+		return res.status(404).send(JSON.stringify({
+			content: 'not found'
+		}))
+	}
+	const server = Server.list[req.params.serverId]
+	if(!server.machine.isOnline){
+		return res.status(500).send(JSON.stringify({
+			content: 'machine offline'
+		}))
+	}
+	if(server.isOnline){
+		return res.status(400).send(JSON.stringify({
+			content: 'already started'
+		}))
+	}
+	server.start()
+	res.status(200).send(JSON.stringify({
+		content: 'started'
+	}))
+})
