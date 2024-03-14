10ecdce236d0371e89a9151d6adf49c7e2e74f0e
ramune314159265 <ramune314159265@gmail.com>
2024-03-14 11:43:25 +0900

    プレイヤー移動関連実装

--- src/websocket/minecraft/index.ts
+++ src/websocket/minecraft/index.ts
@@ -1,9 +1,29 @@
 import { WebSocketServer } from 'ws'
+import { servers } from '../../server'
+import { MinecraftServer } from '../../server/minecraft'
 
 export const minecraftWsServer = new WebSocketServer({
-	port: 8080,
+	port: 8000,
 })
 
 minecraftWsServer.on('connection', (wsConnection) => {
+	wsConnection.on('error', console.error)
+	wsConnection.on('message', message => {
+		const data = JSON.parse(message.toString())
+		switch (data.type) {
+			case 'player_connected':
+				(servers[data.joinedServerId] as MinecraftServer).players.connect(data.playerId)
+				break
+			case 'player_moved':
+				{ (servers[data.previousJoinedServerId] as MinecraftServer).players.disconnect(data.playerId) }
+				{ (servers[data.joinedServerId] as MinecraftServer).players.connect(data.playerId) }
+				break
+			case 'player_disconnected':
+				(servers[data.previousJoinedServerId] as MinecraftServer).players.disconnect(data.playerId)
+				break
 
+			default:
+				break
+		}
+	})
 })
