66d2b3c6255c1507914e65446d0c39a36014c635
ramune314159265 <ramune314159265@gmail.com>
2024-03-14 17:26:00 +0900

    移動

---
+++ src/discord/interfaces.ts
@@ -0,0 +1,16 @@
+import { ColorResolvable } from 'discord.js'
+
+export interface colors {
+	join: ColorResolvable;
+	move: ColorResolvable;
+	leave: ColorResolvable;
+	death: ColorResolvable;
+	normalAdvancement: ColorResolvable;
+	challengeAdvancement: ColorResolvable;
+}
+
+export interface config {
+	token: string,
+	noticeChannelId: string,
+	colors: colors
+}
