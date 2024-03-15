637c74d5ecc77368497d51ee46782153ff012c3b
ramune314159265 <ramune314159265@gmail.com>
2024-03-15 23:25:44 +0900

    更新ボタン追加

--- src/discord/commands/list/index.ts
+++ src/discord/commands/list/index.ts
@@ -1,8 +1,35 @@
-import { APIEmbedField, CommandInteraction, EmbedBuilder } from 'discord.js'
+import { APIEmbedField, ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, EmbedBuilder } from 'discord.js'
 import { client } from '../..'
 import { servers } from '../../../server'
 import { MinecraftServer } from '../../../server/minecraft'
 import { statusEmojis } from '../../../util/server'
+
+const returnServerFieldsEmbed = (canUseReloadButton: boolean): EmbedBuilder => {
+	const serverFields: Array<APIEmbedField> = []
+	for (const server of Object.values(servers)) {
+		if (!(server instanceof MinecraftServer)) {
+			continue
+		}
+		const field: APIEmbedField = {
+			name: `${statusEmojis[server.status] ?? statusEmojis.unknown} ${server.name}`,
+			value: `${server.status === 'online' ? `オンライン プレイヤーリスト:${server.players.list.join(', ')}` : 'オフライン'} `
+		}
+		serverFields.push(field)
+	}
+	const embed = new EmbedBuilder()
+		.setTitle('サーバー情報')
+		.setAuthor(
+			{
+				name: 'ラムネサーバー',
+				iconURL: client.user?.displayAvatarURL()
+			}
+		)
+		.setFields(...serverFields)
+		.setColor('#58b058')
+		.setTimestamp()
+		.setFooter({ text: `※現時点での情報です。更新するには${canUseReloadButton ? '下の更新ボタンを押してください' : 'もう一度/listと入力してください'}` })
+	return embed
+}
 
 export default {
 	data: {
@@ -10,29 +37,43 @@ export default {
 		description: "サーバーの情報を表示します",
 	},
 	async execute(interaction: CommandInteraction) {
-		const serverFields: Array<APIEmbedField> = []
-		for (const server of Object.values(servers)) {
-			if (!(server instanceof MinecraftServer)) {
-				continue
-			}
-			const field: APIEmbedField = {
-				name: `${statusEmojis[server.status] ?? statusEmojis.unknown} ${server.name}`,
-				value: `${server.status === 'online' ? 'オンライン' : 'オフライン'} プレイヤーリスト:${server.players.list.join(', ')}`
-			}
-			serverFields.push(field)
-		}
-		const embed = new EmbedBuilder()
-			.setTitle('サーバー情報')
-			.setAuthor(
-				{
-					name: 'ラムネサーバー',
-					iconURL: client.user?.displayAvatarURL()
-				}
-			)
-			.setFields(...serverFields)
-			.setColor('#58b058')
-			.setTimestamp()
-			.setFooter({ text: '※現時点での情報です。更新するにはもう一度/listと入力してください' })
-		await interaction.reply({ embeds: [embed], ephemeral: true })
+		const message = await interaction.reply({
+			embeds: [returnServerFieldsEmbed(true)],
+			components: [
+				new ActionRowBuilder<ButtonBuilder>()
+					.addComponents(
+						new ButtonBuilder()
+							.setStyle(ButtonStyle.Primary)
+							.setCustomId('reload')
+							.setLabel('更新')
+							.setEmoji('🔃')
+					)
+			],
+			ephemeral: true,
+		})
+		const collector = message.createMessageComponentCollector({ time: 14 * 50 * 1000 /*約15分間(メッセージ更新が出来る期間)*/ })
+
+		collector.on('collect', async (buttonInteraction: ButtonInteraction) => {
+			buttonInteraction.update({
+				embeds: [returnServerFieldsEmbed(true)]
+			})
+		})
+
+		collector.on('end', () => {
+			message.edit({
+				embeds: [returnServerFieldsEmbed(false)],
+				components: [
+					new ActionRowBuilder<ButtonBuilder>()
+						.addComponents(
+							new ButtonBuilder()
+								.setStyle(ButtonStyle.Secondary)
+								.setCustomId('reload')
+								.setLabel('更新')
+								.setEmoji('🔃')
+								.setDisabled(true)
+						)
+				]
+			})
+		})
 	}
 }
