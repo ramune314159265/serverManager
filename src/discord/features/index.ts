548e4c412983cf455351cba736bb0b71b1632e2f
ramune314159265 <ramune314159265@gmail.com>
2024-03-15 23:53:05 +0900

    presenceを更新するように

--- src/discord/features/index.ts
+++ src/discord/features/index.ts
@@ -1,1 +1,2 @@
 import './serverChat'
+import './updatePresence'
---
+++ src/discord/features/updatePresence.ts
@@ -0,0 +1,9 @@
+import { ActivityType } from 'discord.js'
+import { client } from '..'
+
+const updatePresence = () => {
+    client.user?.setActivity?.(`サーバー | ${client.ws.ping === -1 ? '不明' : client.ws.ping}ms`, { type: ActivityType.Playing })
+}
+
+setInterval(updatePresence, 30 * 60 * 1000)
+updatePresence()
