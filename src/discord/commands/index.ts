0cf12df08534aa077c5d4978ad827149ae4ef75a
ramune314159265 <ramune314159265@gmail.com>
2024-03-15 22:58:02 +0900

    リストに追加

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,6 +1,10 @@
 import { ApplicationCommandDataResolvable, CommandInteraction } from 'discord.js'
 
 export const commandsDataArray = [
+	await import('./address/index'),
+	await import('./list/index'),
+	await import('./ping/index'),
+	await import('./realoniserver/index'),
 	await import('./start/index')
 ].map(i => i.default)
 
