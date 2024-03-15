38c55b94e9efaa3849f7b68e0cf7a880d83f60a2
ramune314159265 <ramune314159265@gmail.com>
2024-03-15 16:38:20 +0900

    削除

--- src/websocket/minecraft/index.ts
+++ src/websocket/minecraft/index.ts
@@ -10,27 +10,10 @@ minecraftWsServer.on('connection', (wsConnection) => {
 	wsConnection.on('error', console.error)
 	wsConnection.on('message', message => {
 		const data = JSON.parse(message.toString())
-		switch (data.type) {
-			case 'player_connected':
-				(servers[data.joinedServerId] as MinecraftServer).players.connect(data.playerId)
-				break
-			case 'player_moved':
-				{ (servers[data.previousJoinedServerId] as MinecraftServer).players.disconnect(data.playerId) }
-				{ (servers[data.joinedServerId] as MinecraftServer).players.connect(data.playerId) }
-				break
-			case 'player_disconnected':
-				(servers[data.previousJoinedServerId] as MinecraftServer).players.disconnect(data.playerId)
-				break
-			case 'server_started':
-				(servers[data.serverId] as MinecraftServer).status = 'online'
-				break
-			case 'server_stopped':
-				(servers[data.serverId] as MinecraftServer).status = 'booting'
-				break
-
-			default:
-				break
+		if (data.type !== 'ws_connected') {
+			return
 		}
+		(servers[data.serverId] as MinecraftServer).setWsConnection(wsConnection)
 	})
 })
 
