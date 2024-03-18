8c3fee2375e6533abc47d0552031e1a0b2267bb0
ramune314159265 <ramune314159265@gmail.com>
2024-03-18 17:39:44 +0900

    名前変更

--- src/discord/commands/address/index.ts
+++ src/discord/commands/address/index.ts
@@ -8,15 +8,15 @@ export default {
 	},
 	async execute(interaction: CommandInteraction) {
 		await interaction.deferReply({ ephemeral: true })
-		const IP = await (await fetch('https://ipinfo.io/ip')).text()
+		const ip = await (await fetch('https://ipinfo.io/ip')).text()
 		const embed = new EmbedBuilder()
 			.setTitle('サーバーアドレス')
 			.setAuthor({ name: 'ラムネサーバー' })
 			.setDescription('入れない時は2,3種類のうちの別のアドレスを試してみてください')
 			.setColor('#58b058')
 			.addFields(
-				{ name: '統合版', value: `アドレス(address): \`${IP}\` ポート: \`19132\`\n\nアドレス(address): \`${discordBotConfig.addresses.serverAddress1}\` ポート: \`19132\`` },
-				{ name: 'JAVA版', value: `\`${discordBotConfig.addresses.serverAddress1}\`\n\n\`${IP}\`\n\n\`${discordBotConfig.addresses.serverAddress2}\`` },
+				{ name: '統合版', value: `アドレス(address): \`${ip}\` ポート: \`19132\`\n\nアドレス(address): \`${discordBotConfig.addresses.serverAddress1}\` ポート: \`19132\`` },
+				{ name: 'JAVA版', value: `\`${discordBotConfig.addresses.serverAddress1}\`\n\n\`${ip}\`\n\n\`${discordBotConfig.addresses.serverAddress2}\`` },
 				{ name: 'サバイバルサーバーマップ', value: `https://${discordBotConfig.addresses.serverAddress1}/map/` },
 				{ name: 'データパックサーバーマップ', value: `https://${discordBotConfig.addresses.serverAddress1}/` },
 			)
