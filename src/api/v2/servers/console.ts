c68a6339528eae79fd755f9ae7208ed569bb581a
ramune314159265 <ramune314159265@gmail.com>
2024-05-05 11:14:35 +0900

    sizeなどをrootで返すように

--- src/api/v2/servers/console.ts
+++ src/api/v2/servers/console.ts
@@ -5,6 +5,32 @@ import { WithServerIdParams } from './server'
 
 export const consoleRouter = express.Router({ mergeParams: true })
 
+const rootPath = '/' as const
+
+consoleRouter.get<typeof rootPath, WithServerIdParams<typeof rootPath>>(rootPath, (req, res) => {
+	if (!Object.hasOwn(servers, req.params.serverId)) {
+		return res.status(404).send({
+			content: 'not found'
+		})
+	}
+	const server = servers[req.params.serverId]
+	if (!server.machine.isOnline) {
+		return res.status(500).send({
+			content: 'machine offline'
+		})
+	}
+	if (server.status === 'offline') {
+		return res.status(400).send({
+			content: 'server offline'
+		})
+	}
+	res.status(200).send({
+		id: server.id,
+		col: server.consoleCol,
+		row: server.consoleRow
+	})
+})
+
 const historyPath = '/history' as const
 
 consoleRouter.get<typeof historyPath, WithServerIdParams<typeof historyPath>>(historyPath, (req, res) => {
@@ -27,31 +53,5 @@ consoleRouter.get<typeof historyPath, WithServerIdParams<typeof historyPath>>(hi
 	res.status(200).send({
 		id: server.id,
 		content: server.consoleBuffer
-	})
-})
-
-const sizePath = '/size' as const
-
-consoleRouter.get<typeof sizePath, WithServerIdParams<typeof sizePath>>(sizePath, (req, res) => {
-	if (!Object.hasOwn(servers, req.params.serverId)) {
-		return res.status(404).send({
-			content: 'not found'
-		})
-	}
-	const server = servers[req.params.serverId]
-	if (!server.machine.isOnline) {
-		return res.status(500).send({
-			content: 'machine offline'
-		})
-	}
-	if (server.status === 'offline') {
-		return res.status(400).send({
-			content: 'server offline'
-		})
-	}
-	res.status(200).send({
-		id: server.id,
-		col: server.consoleCol,
-		row: server.consoleRow
 	})
 })
