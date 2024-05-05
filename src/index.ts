63c4c80f160a9813cf008f9c89bbe09f689bb39c
ramune314159265 <ramune314159265@gmail.com>
2024-05-05 21:49:53 +0900

    utilも最初にimportするように

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import './api/index'
 import './server/index'
 import './websocket/child/index'
 import './discord/index'
+import './util/index'
 
 process.on('uncaughtException', err => console.error('uncaughtException:', err))
---
+++ src/util/index.ts
@@ -0,0 +1,5 @@
+import './dice'
+import './japanese'
+import './minecraft'
+import './minimessage'
+import './server'
