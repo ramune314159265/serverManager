35b85b703c16f40800f39a19dc026800751a71a8
ramune314159265 <ramune314159265@gmail.com>
2024-08-16 00:34:12 +0900

    PlayersにSetを使うように

--- src/server/minecraft/players.ts
+++ src/server/minecraft/players.ts
@@ -1,12 +1,16 @@
 export class Players {
-	list: Array<string>
+	#list: Set<string>
+	get list(){
+		return [...this.#list]
+	}
 	constructor(playerList: Array<string> = []) {
-		this.list = playerList
+		this.#list = new Set()
+		playerList.forEach(player => this.#list.add(player))
 	}
 	connect(playerId: string) {
-		this.list.push(playerId)
+		this.#list.add(playerId)
 	}
 	disconnect(playerId: string) {
-		this.list = this.list.filter(id => !(playerId === id))
+		this.#list.delete(playerId)
 	}
 }
